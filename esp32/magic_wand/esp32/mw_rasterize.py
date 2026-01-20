aa977e9ee22a1ac1a754d11ed7cc857c229a4143
Uli Raich <uli.raich@gmail.com>
2026-01-20 17:12:25 +0100

    modified mw_rasterize to make its use with measured data easier

--- esp32/magic_wand/esp32/mw_rasterize.py
+++ esp32/magic_wand/esp32/mw_rasterize.py
@@ -18,7 +18,18 @@ def findStroke(digit_no):
         if strokes["strokes"][i]["label"] == str(digit_no):
             print("Stroke {:d} found!".format(digit_no))
     return strokes["strokes"][digit_no]["strokePoints"]
-        
+
+def toFloatArray(strokePoints):
+    # takes stroke points in form of dictionaries:
+    # {"x": "x_value","y": "y_value"}
+    # and converts them into a list of float values of the form:
+    # x0_value,y0_value,x1_value,y1_value ...
+    pointArray = []
+    for i in range(len(strokePoints)):
+        pointArray.append(float(strokePoints[i]["x"]))
+        pointArray.append(float(strokePoints[i]["y"]))
+    return pointArray
+    
 #rasterizes the stroke
 
 FIXED_POINT = const(256)
@@ -68,26 +79,27 @@ def rasterizeStroke(stroke_points, x_range, y_range, width, height):
     x_range_fp = float_to_fp(x_range)
     y_range_fp = float_to_fp(y_range)
     
-    t_inc_fp = FIXED_POINT / len(stroke_points)
+    t_inc_fp = FIXED_POINT / (len(stroke_points)//2)
     
     one_half_fp = (FIXED_POINT / 2)
 
     # Go through all the stroke points and extract the start x,y, end x,y and the distance in x and y
     # The start point is the point at the current point index, end point is the next point
-
-    for point_index in range(len(stroke_points) - 1):
-        start_point = stroke_points[point_index]
-        end_point = stroke_points[point_index + 1]
-        start_x_fp = norm_to_coord_fp(float(start_point["x"]), x_range_fp, half_width_fp)
-        start_y_fp = norm_to_coord_fp(-float(start_point["y"]), y_range_fp, half_height_fp)
-        end_x_fp = norm_to_coord_fp(float(end_point["x"]), x_range_fp, half_width_fp)
-        end_y_fp = norm_to_coord_fp(-float(end_point["y"]), y_range_fp, half_height_fp)
+    for point_index in range(len(stroke_points)//2 - 2):
+        start_point_x = stroke_points[2*point_index]
+        start_point_y = stroke_points[2*point_index + 1]
+        end_point_x = stroke_points[2*point_index + 2]
+        end_point_y = stroke_points[2*point_index + 3]
+        start_x_fp = norm_to_coord_fp(start_point_x, x_range_fp, half_width_fp)
+        start_y_fp = norm_to_coord_fp(-start_point_y, y_range_fp, half_height_fp)
+        end_x_fp = norm_to_coord_fp(end_point_x, x_range_fp, half_width_fp)
+        end_y_fp = norm_to_coord_fp(-end_point_y, y_range_fp, half_height_fp)
         delta_x_fp = end_x_fp - start_x_fp
         delta_y_fp = end_y_fp - start_y_fp
         t_fp = point_index * t_inc_fp
-        if point_index == 0:
-            print("start_point x: {:5.3f}, y: {:5.3f}, endpoint x: {:5.3f}, y: {:5.3f}".format(
-            start_x_fp,start_y_fp,end_x_fp,end_y_fp))
+        # if point_index == 0:
+            # print("start_point x: {:5.3f}, y: {:5.3f}, endpoint x: {:5.3f}, y: {:5.3f}".format(
+            # start_x_fp,start_y_fp,end_x_fp,end_y_fp))
         if t_fp < one_half_fp:
             local_t_fp = div_fp(t_fp, one_half_fp)
             one_minus_t_fp = FIXED_POINT - local_t_fp
@@ -105,8 +117,8 @@ def rasterizeStroke(stroke_points, x_range, y_range, width, height):
         green = gate(green, 0, 255)
         blue = gate(blue, 0, 255)
 
-        if (point_index == 0):
-            print("red,green,blue: ",red, green, blue)
+        # if (point_index == 0):
+        #     print("red,green,blue: ",red, green, blue)
 
         if abs(delta_x_fp) > abs(delta_y_fp):
             line_length = abs(round_fp_to_int(delta_x_fp))
@@ -133,9 +145,9 @@ def rasterizeStroke(stroke_points, x_range, y_range, width, height):
             if (x < 0) or (x >= width) or (y < 0) or (y >= height):
                 continue
             buffer_index = (y * width * num_channels) + (x * num_channels)
-            if point_index == 0:
-                print("buffer index: ",buffer_index," length of buffer: ",len(buffer))
-                print("Color value at buffer index: ({:d},{:d},{:d})".format(red,green,blue))
+            # if point_index == 0:
+            #     print("buffer index: ",buffer_index," length of buffer: ",len(buffer))
+            #     print("Color value at buffer index: ({:d},{:d},{:d})".format(red,green,blue))
             buffer[buffer_index + 0] = red
             buffer[buffer_index + 1] = green
             buffer[buffer_index + 2] = blue
@@ -154,7 +166,9 @@ except:
 
 for digit in range(10):
     strokePoints = findStroke(digit)
-    rasterBuffer = rasterizeStroke(strokePoints, RANGE_X, RANGE_Y, RASTER_WIDTH,RASTER_HEIGHT)
+    pointArray = toFloatArray(strokePoints)
+    # print("x0: {:5.3f}, y0: {:5.3f}".format(pointArray[0],pointArray[1]))
+    rasterBuffer = rasterizeStroke(pointArray, RANGE_X, RANGE_Y, RASTER_WIDTH,RASTER_HEIGHT)
     '''
     print("Colors:")
     for i in range(4):
