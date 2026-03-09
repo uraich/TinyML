f94be05c79891999c77bcc5f15d5252cebbfc846
Uli Raich <uli.raich@gmail.com>
2026-03-09 11:25:21 +0100

    added plott_strokes.py

---
+++ esp32/magic_wand/test_data/plot_strokes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+stroke_pixels = []
+for i in range(10):
+    f = open("{:d}.bin".format(i),"rb")
+    stroke_pixels.append(np.frombuffer(f.read(),dtype=np.uint8).reshape((32,32,3)))
+
+fig,ax = plt.subplots(2,5)
+fig.suptitle("Stroke test data for all 10 digits")
+for i in range(2):
+    for j in range(5):
+        ax[i,j].set_axis_off()
+        ax[i,j].imshow(stroke_pixels[i*5+j])
+plt.show()
